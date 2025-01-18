d85c2a1a4b75655d0c0ee6c6ae2a140c539dd318
Rolands Kundzins <rolis.kungs@inbox.lv>
2025-01-18 05:39:35 +0200

    Adding debug toolbar

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-"# gym-app" 
+# gym-app
+This app is built using Python Django (frontend not implemented yet).
+
+## To start the project
+```
+python manage.py startserver
+```
--- gym_project/settings.py
+++ gym_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+    'localhost',
 ]
 
 ROOT_URLCONF = 'gym_project.urls'
