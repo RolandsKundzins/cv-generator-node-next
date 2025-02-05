d21b963b3d4ed56387fb01b398341b6d11c6e44d
Rolands Kundzins <rolis.kungs@inbox.lv>
2025-02-05 10:14:10 +0200

    dummy test for github actions

---
+++ node_backend/tests/app.test.js
@@ -0,0 +1,6 @@
+import { test } from 'node:test'; // Import the `test` module
+import assert from 'node:assert'; // Import the `assert` module for assertions
+
+test('Dummy test', async () => {
+  assert.strictEqual(2, 2);
+});
